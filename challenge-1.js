08c569e70c5d9e662d8910146f54d39bd0d641ab
Gary Yamashita <garyjyam@gmail.com>
2026-03-01 08:44:52 -0800

    stretch goals cleaned up

--- challenge-1.js
+++ challenge-1.js
@@ -8,56 +8,57 @@ let hasCriminalRecord = false;
 let eligibleForAccount;
 
 if (age >= 18 && isCitizen && !hasCriminalRecord) {
-    console.log("Account Approved");
-    eligibleForAccount = true;
+  console.log("Account Approved");
+  eligibleForAccount = true;
 } else {
-    console.log("Account Denied");
-    eligibleForAccount = false;
-};
+  console.log("Account Denied");
+  eligibleForAccount = false;
+}
 
 let qualifiedForPremium;
 
 if (eligibleForAccount) {
-    if (annualIncome > 100000 && creditScore > 720) {
-        console.log("Premium Approved");
-        qualifiedForPremium = true;
-    } else {
-        console.log("Standard Account Only");
-        qualifiedForPremium = false;
-    };
+  if (annualIncome > 100000 && creditScore > 720) {
+    console.log("Premium Approved");
+    qualifiedForPremium = true;
+  } else {
+    console.log("Standard Account Only");
+    qualifiedForPremium = false;
+  }
 } else {
-    console.log("Account Denied");
+  console.log("Account Denied");
 }
 
 let flaggedForRisk;
 
 if (eligibleForAccount) {
-
-    if (creditScore < 550 || annualIncome < 25000) {
-        console.log("Flag for Review");
-        flaggedForRisk = true;
-    } else {
-        console.log("No Risk Flags");
-        flaggedForRisk = false;
-    }
+  if (creditScore < 550 || annualIncome < 25000) {
+    console.log("Flag for Review");
+    flaggedForRisk = true;
+  } else {
+    console.log("No Risk Flags");
+    flaggedForRisk = false;
+  }
 } else {
-    console.log("Account Denied");
+  console.log("Account Denied");
 }
 
 // Stretch Goals
 
 let riskLevel;
 
-if (eligibleForAccount) {
+if (!eligibleForAccount) {
+  console.log("Account Denied\nbut,\n");
+}
 
-    if (creditScore < 550) {
-        riskLevel = "High";
-    } else if (creditScore > 550 && creditScore < 650) {
-        riskLevel = "Medium";
-    } else {
-        riskLevel = "Low";
-    }
-} 
-console.log(`${firstName} has a credit score of ${creditScore}, so ${firstName} has a risk level of ${riskLevel}.`);
+if (creditScore < 550) {
+  riskLevel = "High";
+} else if (creditScore > 550 && creditScore < 650) {
+  riskLevel = "Medium";
+} else {
+  riskLevel = "Low";
+}
 
-
+console.log(
+  `${firstName} has a credit score of ${creditScore}, so ${firstName} has a risk level of ${riskLevel}.`,
+);
